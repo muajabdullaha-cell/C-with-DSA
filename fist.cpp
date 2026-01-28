741744390e43d6aa2bcfb77eb4288b3f57bcda5c
muajabdullaha-cell <muajabdullaha@gmail.com>
2026-01-28 22:59:59 +0600

    Initial commit

---
+++ fist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Abdullah";
+    int age = 20;
+    float height = 5.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
