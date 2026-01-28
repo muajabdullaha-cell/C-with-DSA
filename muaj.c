6c8aff06682a0050ac9f257a77313bfe393e45a3
muajabdullaha-cell <muajabdullaha@gmail.com>
2026-01-28 23:05:46 +0600

    create new file

---
+++ muaj.c
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "muaj abdullah";
+    int age = 10000000;
+    float height = 6.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
