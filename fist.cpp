46696346619d9f0032cdd4ad22a5d5acf8b5a383
muajabdullaha-cell <muajabdullaha@gmail.com>
2026-02-01 02:17:13 +0600

    Refactor logical expressions in fist.cpp for improved readability

--- fist.cpp
+++ fist.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     cout << !(3<5) << endl;
     cout << (3>5) << endl;
     cout << (3<=3) << endl;
     cout << (3==5) << endl;
-    cout << (  (3>1)||(3>1) ) << endl;
-    cout << ( (3>1)&&(3>1) ) << endl;
+    cout << ((3>1)||(3>1)) << endl;
+    cout << ((3>1)&&(3>1)) << endl;
+    cout << ((5>2)^(3>1)) << endl;
     return 0;
 }
-
-
