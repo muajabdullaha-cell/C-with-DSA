82fe7f7c81df1c9a2171a6efea2d492626f7d56e
muajabdullaha-cell <muajabdullaha@gmail.com>
2026-02-02 01:48:28 +0600

    Add multiple C++ source files for basic input/output and conditional statements

---
+++ Asfak/variable/variable.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello"<<"Apna college"<< endl;
+    return 0;
+}
---
+++ conditional state/n.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   int age;
+   cout << "enter your age: ";
+   cin >>age;
+
+if (age>=18) {
+    cout << "you are eligble for vote ";
+}else{
+    cout << "you are not eliglble for vote ";
+}
+
+
+
+    return 0;
+
+}
+
+
+
+
+
