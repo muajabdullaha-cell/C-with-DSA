86df5fa8fde6116d29dabfb9b17a1ae4c479be26
Md Muaj Abdullah <muajabdullaha@gmail.com>
2026-02-05 13:24:25 +0600

    patternn qs

--- loops/square pattern/ns.cpp
+++ loops/square pattern/ns.cpp
@@ -28,17 +28,76 @@ using namespace std;
 //     return 0;
 // }
 
-int main () {
-    int n =5;
-    int num = 1;
-    for ( int i=1;i<=n;i++) {
-        cout<<i<< " ";
-        for (int j=1;j<=n;j++) {
-            cout<<num<<" ";
-            num++;
-        }
-        cout<<endl;
-    }
-    return 0;
-}
+// int main () {
+//     int n =5;
+//     int num = 1;
+//     for ( int i=1;i<=n;i++) {
+//         cout<<i<< " ";
+//         for (int j=1;j<=n;j++) {
+//             cout<<num<<" ";
+//             num++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//  int main () {
+//     int n =12;
+//     for ( int i=1;i<=n;i++) {
+//         cout<<i<< " ";
+//         for (int j=1;j<=i+1;j++) {
+//             cout<<" *";
+        
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// } easy wit star pront
+// int main () {
+//     int n =5;
+//     for ( int i=1;i<=n;i++) {
+//         cout<<i<< " ";
+//         for (int j=1;j<=i+1;j++) {
+//             cout<<(i+1)<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+// int main () {
+//     int n =5;
+//     for ( int i=1;i<=n;i++) {
+//         cout<<i<< " ";
+//         int ch = 'A'+i-1;
+//         for (int j=1;j<=i;j++) {
+//             cout<<(char)ch<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+//     return 0; // CHARACTER PATTERN
+// // }
+// int main () {
+//     int n =5;
 
+//     for ( int i=0;i<n;i++) {
+//         for (int j=1;j<=i+1;j++) {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;// (TRIANGLE PATTERN)
+// }
+
+// int main () {
+//     int n =5;
+
+//     for ( int i=0;i<n;i++) {
+//         for (int j=i+1;j>0;j--) {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// } ReVERSE TRIANGLE PATTERN
