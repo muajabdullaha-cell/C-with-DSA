04e304dc6347b90f7d21d84d55b2d7473b37f456
Md Muaj Abdullah <muajabdullaha@gmail.com>
2026-02-08 00:48:28 +0600

    dsa

--- loops/d.cpp
+++ loops/d.cpp
@@ -1,12 +1,41 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() { 
-    int n=10;
-    int i =1;
-    do {
-        cout << i<<" " ; i++;
-    } while (i<=n);
-    cout<<endl;
-    return 0;
+//  int main () {
+//     int n =4;
+//     int ch='A';
+//     for (int i=0;i<n;i++) {
+//         for (int j=i+1;j>0;j--) {
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+// int main () { 
+//     int n=4;
+//     for (int i=0;i<n;i++) {
+//         char ch ='A'+i;
+//         for (int j=0;j<=i;j++) {
+//             cout<<ch<<" ";
+//             ch--;
+            
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+int main () {
+    int n =4;
+    for(int i=0;i<n;i++) {
+        for (int j=0;j<i;j++) {
+            cout<<" ";
+            for (int j=0;j<n-i;j++) {
+                cout<<i+1<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
 }
--- loops/square pattern/ns.cpp
+++ loops/square pattern/ns.cpp
@@ -101,3 +101,56 @@ using namespace std;
 //     }
 //     return 0;
 // } ReVERSE TRIANGLE PATTERN
+// int main () {
+//     int n =5;
+//     for (int i=n;i>0;i--) {
+//         cout<<i<< " ";
+//         cout<<endl;
+//     }
+// return 0;
+// }
+// int main () {
+//     int n =4;
+//     for ( int i=0;i<n;i++) {
+//         char ch='A';
+//         for (int j=0;j<i+1;j++) {
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+// int main () {
+//     int n =7;
+//     for (int i =0;i<n;i++) {
+//         for(int j =1;j<i+1;j++) {
+//             cout << j<< " ";
+            
+//         }
+//       cout<<endl;  
+//     }
+//     return 0;
+// int main() {
+//     int n =4;
+//     for( int i=0;i<n;i++) {
+//         for (int j=i+1;j>0;j--) {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// // }
+// int main () {
+//     int n =4;
+//     int ch= 'A';
+//     for (int i=0;i<n;i++) {
+//         for (int j =i+1;j>0;j--) {
+    
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
