76d0fb7f759c92889d2f33dc4ffdb2f8f08b741c
Md Muaj Abdullah <muajabdullaha@gmail.com>
2026-02-08 19:08:37 +0600

    problem solve

--- loops/d.cpp
+++ loops/d.cpp
@@ -26,16 +26,76 @@ using namespace std;
 //     }
 //     return 0;
 // }
+// int main () {
+//     int n =4;
+//     for(int i=0;i<n;i++) {
+//         for (int j=0;j<i;j++) {
+//             cout<<" ";
+//             for (int j=0;j<n-i;j++) {
+//                 cout<<i+1<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         return 0;
+//     }
+// }
+// int main () {
+//     int n=10;
+//     for (int i=0;i<n;i++) {
+//         for (int j=0;j<n-i-1;j++) {
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i+1;j++) {
+//             cout<<j;
+//         }
+
+//         for(int j=i;j>0;j--) { 
+//             cout<<j;
+//         }
+//         cout<<endl;
+// }
+// return 0;
+// } pyramid pattern
+// hollo duamond pattern
+// // TOP Part
 int main () {
     int n =4;
-    for(int i=0;i<n;i++) {
-        for (int j=0;j<i;j++) {
-            cout<<" ";
-            for (int j=0;j<n-i;j++) {
-                cout<<i+1<<" ";
-            }
-            cout<<endl;
+    for (int i=0;i<n;i++) {
+        // space
+        for (int j= 0;j<n-i-1;j++) {
+            cout<< " ";
         }
-        return 0;
+        cout<<"*";
+        // space
+        if (i!=0) {
+            for ( int j=0;j<2*i-1;j++) {
+                cout<<" ";
+            }
+            cout<< "*"; 
+        }
+        cout<<endl;
     }
+
+     
+    //  bottom
+    for (int i =0;i<(n-1);i++) {
+        // spaces
+ for (int j= 0;j<i+1;j++) {
+            cout<< " ";
+
+    }
+    cout<< "*";
+       if(i!=n-1) {
+        for ( int j=0;j<2*(n-i)-5;j++) {
+                cout<<" ";
+            }
+            cout<<"*";
+        
+    }
+
+     cout<<endl;
 }
+   return 0;
+}
+
+   
--- loops/f.cpp
+++ loops/f.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    int n =50;
-    for ( int i =2;i<=n;i=i+2) {
-        cout << i << " ";
+// int main () {
+//     int n =50;
+//     for ( int i =2;i<=n;i=i+2) {
+//         cout << i << " ";
 
     
-    }
+//     }
 
-    cout << endl;
-    return 0;
+//     cout << endl;
+//     return 0;
+// }
+int main () {
+    int n =4;
+    for(int i=0;i<n;i++) {
+        for (int j=0;j<i;j++) {
+            cout<<" ";
+            for (int j=0;j<n-i;j++) {
+                cout<<i+1<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
 }
